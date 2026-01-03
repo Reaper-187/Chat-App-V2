7126f9b9c79e7a690c5f922053ce9e7749912d1c
Reaper-187 <Abdulkader_cheik@outlook.de>
2026-01-03 22:03:49 +0100

    feat(socket): - Typing and management of socket connections - Defined local SessionSocket interface for type safety - Implemented single-socket per user - Automatically disconnect old socket connections on re-login - Introduced users map to manage active connections - Added disconnect handling to clean up the map

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,13 +1,26 @@
-import { Server } from "socket.io";
-import { Server as HttpServer } from "http";
+import { Server, Socket } from "socket.io";
+import { Server as HttpServer, IncomingMessage } from "http";
 import { corsSetup, sessionSetup } from "./config/session";
 import { checkUserAuth } from "./middleware/auth.middleware";
 import { checkGuestExpiry } from "./middleware/guest.auth.middleware";
+import { SessionData } from "express-session";
+
 // socket arbeitet nicht mit req,res,next sondern nur mit socket,next daher
 // Wrapper-Funktion
 const wrap = (middleware: any) => (socket: any, next: any) => {
   middleware(socket.request, {} as any, next);
 };
+
+// musste interface in der file schreiben sonst ERRORS OHNE ENDE
+interface SessionIncomingMessage extends IncomingMessage {
+  session: SessionData;
+}
+
+interface SessionSocket extends Socket {
+  request: SessionIncomingMessage;
+}
+
+const users = new Map();
 
 export function initSocket(server: HttpServer) {
   const io = new Server(server, {
@@ -21,17 +34,35 @@ export function initSocket(server: HttpServer) {
       io.use(wrap(checkUserAuth));
       io.use(wrap(checkGuestExpiry));
       next();
-    } catch (err: any | undefined) {
+    } catch (err: any) {
       next(err);
     }
   });
 
-  io.on("connection", (socket) => {
-    console.log("User connected", socket.id);
+  io.on("connection", (defaultSocket: Socket) => {
+    const socket = <SessionSocket>defaultSocket;
+    const userId = socket.request.session.userId;
+
+    // Prüfen ob eine Verbindung existiert
+    const existingSocket = users.get(userId);
+    if (existingSocket) {
+      existingSocket.disconnect();
+    }
+
+    // neue Verbindung setzen
+    users.set(userId, socket);
+
+    console.log("User connected", userId);
 
     socket.on("disconnect", () => {
-      console.log("User disconnected", socket.id);
+      // Map cleanen wenn der User sich trennt
+      if (users.get(userId) === socket) {
+        users.delete(userId);
+      }
+      console.log("User disconnected", userId);
     });
+
+    return console.log("User Connected", userId);
   });
 
   return io;
